d04e51a07fbb2e264c04a4dbd12e50686dec356b
saeidshahbazi <arash9775@gmail.com>
2024-05-29 11:33:17 +0330

    add excell export to payment record list

--- apps/order/api/views.py
+++ apps/order/api/views.py
@@ -344,6 +344,17 @@ class PaymentRecordListView(ListAPIView):
     # set_user = True
     # model_name = 'order'
 
+    def get(self, request, *args, **kwargs):
+        get_list = self.list(request, *args, **kwargs)
+        if self.request.query_params.get('export_excel', 'False').lower() == 'true':
+            file_url = export_excel(self.queryset)
+            if file_url:
+                full_url = self.request.build_absolute_uri(file_url)
+                return Response({'file_url': full_url})
+            return Response({'error': 'Export failed'}, status=500)
+        else:
+            return get_list
+
 
 class PaymentRecordManagerListView(ListAPIView):
     """
