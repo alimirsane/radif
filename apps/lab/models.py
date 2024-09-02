2defc918c593b9aa3bf01c856e96cd39329a61e6
saeidshahbazi <arash9775@gmail.com>
2024-09-02 11:40:58 +0330

    fix bug is completed was unknown choose

--- apps/lab/models.py
+++ apps/lab/models.py
@@ -224,8 +224,8 @@ class Request(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='توضیحات')
     subject = models.CharField(max_length=100, blank=True, null=True, verbose_name='موضوع درخواست')
     request_number = models.CharField(max_length=20, blank=True, verbose_name='شماره درخواست')
-    is_completed = models.BooleanField(default=False, blank=True, null=True, verbose_name='تکمیل شده')
-    sample_check = models.BooleanField(default=False, blank=True, null=True, verbose_name='چک نمونه')
+    is_completed = models.BooleanField(default=False, verbose_name='تکمیل شده')
+    sample_check = models.BooleanField(default=False, verbose_name='چک نمونه')
 
     is_returned = models.BooleanField(default=False, verbose_name='عودت شده')
 
