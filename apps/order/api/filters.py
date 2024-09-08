928106483b27b472fb3b12a51ccac788f1e1cffe
saeidshahbazi <arash9775@gmail.com>
2024-09-09 02:10:46 +0330

    fix bug filter start/end date

--- apps/order/api/filters.py
+++ apps/order/api/filters.py
@@ -4,8 +4,8 @@ from apps.order.models import PaymentRecord
 
 
 class PaymentRecordFilter(django_filters.FilterSet):
-    start_date = django_filters.DateFilter(field_name='created_at', lookup_expr='gte', label='filter by created_at')  # lookup_expr='icontains',
-    end_date = django_filters.DateFilter(field_name='created_at', lookup_expr='lte', label='filter by created_at')  # lookup_expr='icontains',
+    start_date = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='gte', label='filter by created_at')  # lookup_expr='icontains',
+    end_date = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte', label='filter by created_at')  # lookup_expr='icontains',
 
     search = django_filters.CharFilter(method='request_search')
 
