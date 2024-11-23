404a5c28bb4118ec09c8fb0c35eb161c6d404c5e
saeidshahbazi <arash9775@gmail.com>
2024-11-24 02:03:24 +0330

    fix bug request number and status notify

--- apps/lab/signals.py
+++ apps/lab/signals.py
@@ -72,6 +72,11 @@ def create_request_number(sender, instance, created, **kwargs):
             instance.request_number = f'{date_code[1:]}-{month_code:04d}'
             instance.save()
         if not instance.request_number and instance.parent_request:
+            if not instance.parent_request.request_number:
+                date_code = jdatetime.datetime.now().strftime('%Y%m')
+                month_code = instance.parent_request.current_month_counter() + 1
+                instance.parent_request.request_number = f'{date_code[1:]}-{month_code:04d}'
+                instance.parent_request.save()
             child_requests = instance.parent_request.child_requests.all()
             instance.request_number = f'{instance.parent_request.request_number}-{child_requests.count():02d}'
             instance.save()
