19a376b3647cdc1a37b8b541eacdcd9f5c757809
saeidshahbazi <arash9775@gmail.com>
2025-01-07 11:54:58 +0330

    add some field to main report

--- apps/report/api/functions.py
+++ apps/report/api/functions.py
@@ -7,6 +7,9 @@ from django.conf import settings
 import os
 from datetime import datetime
 
+from apps.order.models import PaymentRecord
+
+
 def generate_excel_report():
     data = []
 
@@ -23,6 +26,88 @@ def generate_excel_report():
             'Parent Discount': parent_request.discount,
             'Parent Created At': parent_request.created_at.replace(tzinfo=None) if is_aware(
                 parent_request.created_at) else parent_request.created_at,
+            "نام سازمان": parent_request.owner.company_name if parent_request.owner.account_type == 'business' else "",
+            "شخصیت مشتری": parent_request.owner.account_type,
+            "نوع مشتری": "دانشجو" if parent_request.owner.is_sharif_student else "سایر",
+            "نام مشتری/نماینده قانونی": parent_request.owner.first_name,
+            "نام خانوادگی مشتری/نماینده قانونی": parent_request.owner.last_name,
+            "کدملی مشتری/نماینده قانونی": parent_request.owner.national_id,
+            "شناسه ملی سازمان": parent_request.owner.company_national_id if parent_request.owner.account_type == 'business' else "",
+            "کد اقتصادی سازمان مشتری": parent_request.owner.company_economic_number if parent_request.owner.account_type == 'business' else "",
+            "شماره همراه مشتری/نماینده قانونی": parent_request.owner.username,
+            "ایمیل مشتری": parent_request.owner.email,
+            "نام آزمایشگاه": parent_request.experiment.laboratory.name,
+            "کد کنترلی آزمایشگاه": parent_request.experiment.laboratory.control_code,
+            "نام و نام اختصاری نوع دستگاه": parent_request.experiment.device.name,
+            "برند و مدل دستگاه": parent_request.experiment.device.model,
+            "کد کنترلی دستگاه": parent_request.experiment.device.control_code,
+            "نام آزمون": parent_request.experiment.name,
+            "کد کنترلی آزمون": parent_request.experiment.control_code,
+            "نام استاندارد": "استاندارد مربوطه",  # مقدار پیش‌فرض
+            "کد کنترلی استاندارد": "کد استاندارد",  # مقدار پیش‌فرض
+            "نام پارامتر": "پارامتر مربوطه",  # مقدار پیش‌فرض
+            "نام اپراتور": parent_request.experiment.operator.first_name if parent_request.experiment.operator else "",
+            "نام مدیر آزمایشگاه": parent_request.experiment.laboratory.technical_manager.first_name if parent_request.experiment.laboratory.technical_manager else "",
+            "تعداد نمونه": parent_request.parameter.count(),
+            "تاریخ دریافت نمونه": parent_request.created_at.date() if parent_request.created_at else "",
+            "نوع واحد آزمون(مبنای تعیین تعرفه)": parent_request.experiment.test_unit_type,
+            "تعرفه پارامتر": "",  # مقدار پیش‌فرض
+            "هزینه کل آزمون": parent_request.price_wod,
+            "درصد تخفیف": parent_request.discount,
+            "مبلغ تخفیف": parent_request.price_wod - parent_request.price if parent_request.price_wod and parent_request.price else 0,
+            "هزینه کل آزمون بعد از کسر تخفیف": parent_request.price,
+            "نوع گرنت 1": parent_request.grant_request1.title if parent_request.grant_request1 else "",
+            "مبلغ گرنت 1": parent_request.grant_request1.approved_amount if parent_request.grant_request1 else 0,
+            "نوع گرنت 2": parent_request.grant_request2.title if parent_request.grant_request2 else "",
+            "مبلغ گرنت 2": parent_request.grant_request2.approved_amount if parent_request.grant_request2 else 0,
+            "نوع گرنت 3": "",  # مقدار پیش‌فرض
+            "مبلغ گرنت 3": 0,  # مقدار پیش‌فرض
+            "نوع گرنت 4": "",  # مقدار پیش‌فرض
+            "مبلغ گرنت 4": 0,  # مقدار پیش‌فرض
+            "نوع گرنت 5": "",  # مقدار پیش‌فرض
+            "مبلغ گرنت 5": 0,  # مقدار پیش‌فرض
+            "مجموع مبلغ گرنت": sum([
+                parent_request.grant_request1.approved_amount if parent_request.grant_request1 else 0,
+                parent_request.grant_request2.approved_amount if parent_request.grant_request2 else 0
+            ]),
+            "مبلغ کل پرداختی": parent_request.price - sum([
+                parent_request.grant_request1.approved_amount if parent_request.grant_request1 else 0,
+                parent_request.grant_request2.approved_amount if parent_request.grant_request2 else 0
+            ]),
+            "نوع پرداخت 1": "",  # مقدار پیش‌فرض
+            "مبلغ پرداخت 1": 0,  # مقدار پیش‌فرض
+            "شماره تراکنش 1": "",  # مقدار پیش‌فرض
+            "تاریخ پرداخت 1": "",  # مقدار پیش‌فرض
+            "نوع پرداخت 2": "",  # مقدار پیش‌فرض
+            "مبلغ پرداخت 2": 0,  # مقدار پیش‌فرض
+            "شماره تراکنش 2": "",  # مقدار پیش‌فرض
+            "تاریخ پرداخت 2": "",  # مقدار پیش‌فرض
+            "نوع پرداخت 3": "",  # مقدار پیش‌فرض
+            "مبلغ پرداخت 3": 0,  # مقدار پیش‌فرض
+            "شماره تراکنش 3": "",  # مقدار پیش‌فرض
+            "تاریخ پرداخت 3": "",  # مقدار پیش‌فرض
+            "نوع پرداخت 4": "",  # مقدار پیش‌فرض
+            "مبلغ پرداخت 4": 0,  # مقدار پیش‌فرض
+            "شماره تراکنش 4": "",  # مقدار پیش‌فرض
+            "تاریخ پرداخت 4": "",  # مقدار پیش‌فرض
+            "نوع پرداخت 5": "",  # مقدار پیش‌فرض
+            "مبلغ پرداخت 5": 0,  # مقدار پیش‌فرض
+            "شماره تراکنش 5": "",  # مقدار پیش‌فرض
+            "تاریخ پرداخت 5": "",  # مقدار پیش‌فرض
+            "تاریخ ثبت موقت درخواست": parent_request.created_at.date() if parent_request.created_at else "",
+            "تاریخ ثبت نهایی درخواست": parent_request.updated_at.date() if parent_request.updated_at else "",
+            "تاریخ تعهد تحویل به مشتری": parent_request.delivery_date,
+            "تاریخ انجام آزمون توسط اپراتور": "",  # مقدار پیش‌فرض
+            "تاریخ تایید مدیر آزمایشگاه": "",  # مقدار پیش‌فرض
+            "مهلت زمانی توقف به روز(از تاریخ ثبت اولیه درخواست تا تاریخ ثبت نهایی)": (
+                        parent_request.updated_at - parent_request.created_at).days if parent_request.updated_at and parent_request.created_at else 0,
+            "مهلت زمانی مقرر به روز(از تاریخ ثبت نهایی تا تاریخ تعهد تحویل به مشتری)": (
+                        parent_request.delivery_date - parent_request.updated_at.date()).days if parent_request.delivery_date and parent_request.updated_at else 0,
+            "مدت زمان توقف به روز(از تاریخ ثبت نهایی تا تاریخ تایید مدیر آز)": 0,  # مقدار پیش‌فرض
+            "مدت زمان تاخیر به روز (مدت زمان توقف از مهلت زمانی مقرر کسر شود)": 0,  # مقدار پیش‌فرض
+            "شماره درخواست": parent_request.request_number,
+            "وضعیت": parent_request.lastest_status().step.name if parent_request.lastest_status() else "",
+            "نام هماهنگ کننده": parent_request.owner.first_name if parent_request.owner else "",
         }
 
         # Fetch child requests for the parent request
@@ -52,6 +137,10 @@ def generate_excel_report():
             transactions = GrantTransaction.objects.filter(grant_record__receiver=parent_request.owner)
             transactions_total = transactions.aggregate(total_amount=Sum('amount'))['total_amount'] or 0
             combined_data['Parent Transactions Total'] = transactions_total
+
+            transactions = PaymentRecord.objects.filter(order__request=parent_request)
+            transactions_total = transactions.aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+            combined_data['Parent PaymentRecord Total'] = transactions_total
 
             # Add row to data
             data.append(combined_data)
