744d1c7a7a8f4fdd5d45af179265d19367f03e53
saeidshahbazi <arash9775@gmail.com>
2025-01-13 15:16:58 +0330

    improve exception handler

--- SLIMS/exception.py
+++ SLIMS/exception.py
@@ -4,12 +4,17 @@ from rest_framework.views import exception_handler
 def slims_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
-        errors = []
-        message = response.data.get('detail')
-        if not message:
-            for field, value in response.data.items():
-                errors.append("{} : {}".format(field, " ".join(value)))
+        if isinstance(response.data, dict):
+            errors = []
+            for field, messages in response.data.items():
+                if isinstance(messages, list):
+                    for message in messages:
+                        errors.append(f"{field}: {message}")
+                else:
+                    errors.append(f"{field}: {messages}")
             response.data = {'data': [], 'message': 'Validation error', 'errors': errors}
+        elif isinstance(response.data, list):
+            response.data = {'data': [], 'message': 'Validation error', 'errors': response.data}
         else:
-            response.data = {'data': [], 'message': 'An error occurred', 'errors': [message]}
+            response.data = {'data': [], 'message': 'An unknown error occurred', 'errors': [str(response.data)]}
     return response
