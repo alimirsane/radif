df6e7947f592b440737a2be1046db6bf1603bc5a
saeidshahbazi <arash9775@gmail.com>
2025-04-14 00:04:19 +0330

    fix bug celery

--- SLIMS/celery.py
+++ SLIMS/celery.py
@@ -3,12 +3,12 @@ import os
 from celery import Celery
 
 # تنظیمات دجانگو
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'envs.common')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SLIMS.envs.common')
 
 app = Celery('celery')
 
 # بارگذاری تنظیمات از فایل تنظیمات دجانگو
-app.config_from_object('django.conf:envs.common', namespace='CELERY')
+app.config_from_object('django.conf:SLIMS.envs.common', namespace='CELERY')
 
 # خودکار کشف کردن تسک‌ها در برنامه‌های Django
 app.autodiscover_tasks()
