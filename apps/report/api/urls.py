6a509e27b4d59343a2c0c14de5de58c9006dca4a
saeidshahbazi <arash9775@gmail.com>
2025-07-07 15:53:28 +0330

    add lab operator excel report service

--- apps/report/api/urls.py
+++ apps/report/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.report.api.views import ExcelReportAPIView, LaboratoryExcelReportAPIView
+from apps.report.api.views import ExcelReportAPIView, LaboratoryExcelReportAPIView, LaboratoryOperatorExcelReportAPIView
 
 urlpatterns = [
     path('main_excel/', ExcelReportAPIView.as_view(), name='main-excel-report-api'),
     path('lab_excel/', LaboratoryExcelReportAPIView.as_view(), name='lab-excel-report-api'),
+    path('lab_opr_excel/', LaboratoryOperatorExcelReportAPIView.as_view(), name='lab_opr-excel-report-api'),
 
 ]
--- apps/report/api/views.py
+++ apps/report/api/views.py
@@ -139,3 +139,99 @@ class LaboratoryExcelReportAPIView(APIView):
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+
+class LaboratoryOperatorExcelReportAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            lab_id = request.query_params.get('lab_id')
+            start_date = request.query_params.get('start_date')
+            end_date = request.query_params.get('end_date')
+
+            try:
+                if start_date:
+                    start_date = make_aware(datetime.datetime.strptime(start_date, '%Y-%m-%d'))
+                if end_date:
+                    end_date = make_aware(datetime.datetime.strptime(end_date, '%Y-%m-%d') + datetime.timedelta(days=1))
+            except Exception:
+                return Response(
+                    {'error': 'Invalid date format. Use YYYY-MM-DD'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            labs = Laboratory.objects.all()
+            if lab_id:
+                labs = labs.filter(id=lab_id)
+
+            data = []
+            for lab in labs:
+                base_requests = Request.objects.filter(
+                    experiment__laboratory=lab,
+                    is_completed=True,
+                    request_status__step__id__exact=8
+                ).distinct()
+
+                if start_date:
+                    base_requests = base_requests.filter(created_at__gte=start_date)
+                if end_date:
+                    base_requests = base_requests.filter(created_at__lt=end_date)
+
+                for operator in lab.operators.all():
+                    requests = base_requests.filter(
+                        request_status__step__id=6,
+                        request_status__action_by=operator
+                    ).distinct()
+                    parent_requests = requests.filter(parent_request__isnull=True)
+                    total_income = parent_requests.aggregate(total=Sum('price'))['total'] or 0
+                    total_income_wod = parent_requests.aggregate(total=Sum('price_wod'))['total'] or 0
+                    total_grant_request_discount = parent_requests.aggregate(total=Sum('grant_request_discount'))['total'] or 0
+                    total_labsnet_discount = parent_requests.aggregate(total=Sum('labsnet_discount'))['total'] or 0
+                    total_request = parent_requests.count()
+                    total_samples = requests.annotate(
+                        total_samples=Sum('formresponse__response_count')
+                    ).aggregate(total=Sum('total_samples'))['total'] or 0
+
+                    data.append({
+                        'نام آزمایشگاه': lab.name,
+                        'اپراتور': operator.get_full_name(),
+                        'تعداد درخواست': total_request,
+                        'تعداد نمونه': total_samples,
+                        'درآمد ناخالص': total_income_wod,
+                        'درآمد': total_income,
+                        'لبزنت': total_labsnet_discount,
+                        'پژوهشی': total_grant_request_discount,
+                    })
+
+            df = pd.DataFrame(data)
+            now_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f'laboratory_operator_report_{now_str}.xlsx'
+            full_path = os.path.join(settings.MEDIA_ROOT, 'reports', filename)
+            os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+            with pd.ExcelWriter(full_path, engine='openpyxl') as writer:
+                df.to_excel(writer, index=False, sheet_name='گزارش اپراتورها')
+
+            relative_path = os.path.relpath(full_path, settings.MEDIA_ROOT)
+            file_url = request.build_absolute_uri(
+                os.path.join(settings.MEDIA_URL, relative_path).replace('\\', '/')
+            )
+
+            return Response(
+                {
+                    "status": "success",
+                    "data": {"url": file_url},
+                    "message": "گزارش اپراتورها با موفقیت تولید شد"
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "status": "error",
+                    "errors": {"error": str(e)},
+                    "message": "خطا در تولید گزارش"
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
