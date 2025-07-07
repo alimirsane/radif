d918e28432441824f460765bda797c34ba002580
saeidshahbazi <arash9775@gmail.com>
2025-07-07 14:56:47 +0330

    fix bug

--- apps/report/api/views.py
+++ apps/report/api/views.py
@@ -55,60 +55,86 @@ class LaboratoryExcelReportAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        # دریافت پارامترهای فیلتر
-        lab_id = request.query_params.get('lab_id')
-        start_date = request.query_params.get('start_date')
-        end_date = request.query_params.get('end_date')
-
         try:
-            if start_date:
-                start_date = make_aware(datetime.datetime.strptime(start_date, '%Y-%m-%d'))
-            if end_date:
-                end_date = make_aware(datetime.datetime.strptime(end_date, '%Y-%m-%d') + datetime.timedelta(days=1))
-        except Exception as e:
-            return Response({'error': 'Invalid date format. Use YYYY-MM-DD'}, status=400)
+            lab_id = request.query_params.get('lab_id')
+            start_date = request.query_params.get('start_date')
+            end_date = request.query_params.get('end_date')
 
-        labs = Laboratory.objects.all()
-        if lab_id:
-            labs = labs.filter(id=lab_id)
+            try:
+                if start_date:
+                    start_date = make_aware(datetime.datetime.strptime(start_date, '%Y-%m-%d'))
+                if end_date:
+                    end_date = make_aware(datetime.datetime.strptime(end_date, '%Y-%m-%d') + datetime.timedelta(days=1))
+            except Exception:
+                return Response(
+                    {'error': 'Invalid date format. Use YYYY-MM-DD'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
-        data = []
-        for lab in labs:
-            requests = Request.objects.filter(
-                experiment__laboratory=lab,
-                is_completed=True,
-                request_status__step__id__exact=8
+            labs = Laboratory.objects.all()
+            if lab_id:
+                labs = labs.filter(id=lab_id)
+
+            data = []
+            for lab in labs:
+                requests = Request.objects.filter(
+                    experiment__laboratory=lab,
+                    is_completed=True,
+                    request_status__step__id__exact=8
+                )
+                if start_date:
+                    requests = requests.filter(created_at__gte=start_date)
+                if end_date:
+                    requests = requests.filter(created_at__lt=end_date)
+
+                total_income = requests.aggregate(total=Sum('price'))['total'] or 0
+                total_income_wod = requests.aggregate(total=Sum('price_wod'))['total'] or 0
+                total_grant_request_discount = requests.aggregate(total=Sum('grant_request_discount'))['total'] or 0
+                total_labsnet_discount = requests.aggregate(total=Sum('labsnet_discount'))['total'] or 0
+                total_request = requests.count()
+                total_samples = requests.annotate(total_samples=Sum('formresponse__response_count')).aggregate(total=Sum('total_samples'))['total'] or 0
+                operators = ', '.join([operator.get_full_name() for operator in lab.operators.all()])
+
+                data.append({
+                    'نام آزمایشگاه': lab.name,
+                    'مدیر فنی': str(lab.technical_manager.get_full_name()) if lab.technical_manager else '',
+                    'اپراتور': str(operators),
+                    'تعداد درخواست': total_request,
+                    'تعداد نمونه': total_samples,
+                    'درآمد ناخالص': total_income_wod,
+                    'درآمد': total_income,
+                    'تخفیف لبزنت': total_labsnet_discount,
+                    'تخفیف پژوهشی': total_grant_request_discount,
+                })
+
+            df = pd.DataFrame(data)
+            now_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f'laboratory_report_{now_str}.xlsx'
+            full_path = os.path.join(settings.MEDIA_ROOT, 'reports', filename)
+            os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+            with pd.ExcelWriter(full_path, engine='openpyxl') as writer:
+                df.to_excel(writer, index=False, sheet_name='گزارش آزمایشگاه‌ها')
+
+            relative_path = os.path.relpath(full_path, settings.MEDIA_ROOT)
+            file_url = request.build_absolute_uri(
+                os.path.join(settings.MEDIA_URL, relative_path).replace('\\', '/')
             )
-            if start_date:
-                requests = requests.filter(created_at__gte=start_date)
-            if end_date:
-                requests = requests.filter(created_at__lt=end_date)
 
-            total_income = requests.aggregate(total=Sum('price'))['total'] or 0
-            total_income_wod = requests.aggregate(total=Sum('price_wod'))['total'] or 0
-            total_grant_request_discount = requests.aggregate(total=Sum('grant_request_discount'))['total'] or 0
-            total_labsnet_discount = requests.aggregate(total=Sum('labsnet_discount'))['total'] or 0
-            total_request = requests.count()
-            total_samples = requests.annotate(total_samples=Sum('formresponse__response_count')).aggregate(total=Sum('total_samples'))['total'] or 0
-            operators = ', '.join([operator.get_full_name() for operator in lab.operators.all()])
-
-            data.append({
-                'نام آزمایشگاه': lab.name,
-                'مدیر فنی': str(lab.technical_manager.get_full_name()) if lab.technical_manager else '',
-                'اپراتور': str(operators) if lab.operators else '',
-                'تعداد درخواست': total_request,
-                'تعداد نمونه': total_samples,
-                'درآمد ناخالص': total_income_wod,
-                'درآمد': total_income,
-                'تخفیف لبزنت': total_labsnet_discount,
-                'تخفیف پزوهشی': total_grant_request_discount,
-            })
-
-        df = pd.DataFrame(data)
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        filename = f'laboratory_report_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.xlsx'
-        response['Content-Disposition'] = f'attachment; filename={filename}'
-        with pd.ExcelWriter(response, engine='openpyxl') as writer:
-            df.to_excel(writer, index=False, sheet_name='گزارش آزمایشگاه‌ها')
-
-        return response
+            return Response(
+                {
+                    "status": "success",
+                    "data": {"url": file_url},
+                    "message": "گزارش با موفقیت تولید شد"
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "status": "error",
+                    "errors": {"error": str(e)},
+                    "message": "خطا در تولید گزارش"
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
